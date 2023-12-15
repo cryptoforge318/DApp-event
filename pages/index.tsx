c130cbf61871e99f83ebffb449f1af8a6e061c25
cryptoforge318 <cryptoforge318@gmail.com>
2023-12-15 11:01:20 +0100

    adds a load more feature

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,17 @@ import { getEvents } from '@/services/blockchain'
 import { EventStruct } from '@/utils/type.dt'
 import { NextPage } from 'next'
 import Head from 'next/head'
+import { useEffect, useState } from 'react'
 
 const Page: NextPage<{ eventsData: EventStruct[] }> = ({ eventsData }) => {
+  const [end, setEnd] = useState<number>(6)
+  const [count] = useState<number>(6)
+  const [collection, setCollection] = useState<EventStruct[]>([])
+
+  useEffect(() => {
+    setCollection(eventsData.slice(0, end))
+  }, [eventsData, end])
+
   return (
     <div>
       <Head>
@@ -14,19 +23,22 @@ const Page: NextPage<{ eventsData: EventStruct[] }> = ({ eventsData }) => {
       </Head>
 
       <Hero />
-      <EventList events={eventsData} />
+      <EventList events={collection} />
 
       <div className="mt-10 h-20 "></div>
 
-      <div className="w-full flex justify-center items-center">
-        <button
-          className="bg-orange-500 shadow-md rounded-full py-3 px-4
+      {collection.length > 0 && eventsData.length > collection.length && (
+        <div className="w-full flex justify-center items-center">
+          <button
+            className="bg-orange-500 shadow-md rounded-full py-3 px-4
         text-white duration-300 transition-all"
-        >
-          {' '}
-          Load More
-        </button>
-      </div>
+            onClick={() => setEnd(end + count)}
+          >
+            {' '}
+            Load More
+          </button>
+        </div>
+      )}
     </div>
   )
 }
