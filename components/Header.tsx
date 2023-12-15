3430fafc84b2ad1d5dbdc69931c56b64e45ef15f
cryptoforge318 <cryptoforge318@gmail.com>
2023-12-15 12:01:19 +0100

    made the header component responsive with a sidebar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,27 +1,78 @@
-import React from 'react'
-import ConnectBtn from './ConnectBtn'
 import Link from 'next/link'
+import ConnectBtn from './ConnectBtn'
+import React, { useState } from 'react'
+import { CgMenuLeft } from 'react-icons/cg'
+import { FaTimes } from 'react-icons/fa'
 
 const Header: React.FC = () => {
   return (
-    <header className="h-20 shadow-md py-5 fixed z-50 top-0 right-0 left-0 bg-white">
+    <header className="h-20 shadow-md p-5 sm:px-0 fixed z-50 top-0 right-0 left-0 bg-white">
       <main className="lg:w-2/3 w-full mx-auto flex justify-between items-center flex-wrap">
         <Link href={'/'} className="text-lg font-bold">
           EventX
         </Link>
+        <Desktop />
+        <Mobile />
+      </main>
+    </header>
+  )
+}
 
-        <div className="flex justify-end items-center space-x-2 md:space-x-4 mt-2 md:mt-0">
+const Desktop: React.FC = () => (
+  <div className="hidden sm:flex justify-end items-center space-x-2 md:space-x-4 mt-2 md:mt-0">
+    <Link
+      href={'/events/create'}
+      className="text-md hover:text-orange-500 duration-300 transition-all"
+    >
+      Create
+    </Link>
+    <Link
+      href={'/events/personal'}
+      className="text-md hover:text-orange-500 duration-300 transition-all"
+    >
+      Personal
+    </Link>
+
+    <ConnectBtn networks />
+  </div>
+)
+
+const Mobile: React.FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  return (
+    <div className="sm:hidden">
+      <button onClick={() => setIsOpen(!isOpen)}>
+        <CgMenuLeft size={25} />
+      </button>
+      {isOpen && (
+        <div
+          className="flex flex-col space-y-4 fixed top-0 right-0 h-full w-64 bg-white
+        shadow-md p-4 transition duration-500 ease-in-out transform-all"
+        >
+          <div className="flex justify-end">
+            <button onClick={() => setIsOpen(!isOpen)}>
+              <FaTimes size={25} />
+            </button>
+          </div>
+
           <Link
             href={'/events/create'}
-            className="text-md hover:text-orange-500 duration-300 transition-all"
+            className="text-md hover:text-orange-500 duration-300 transition-all block py-1"
           >
             Create
           </Link>
 
+          <Link
+            href={'/events/personal'}
+            className="text-md hover:text-orange-500 duration-300 transition-all block py-1"
+          >
+            Personal
+          </Link>
           <ConnectBtn />
         </div>
-      </main>
-    </header>
+      )}
+    </div>
   )
 }
 
---
+++ pages/events/personal.tsx
@@ -0,0 +1,50 @@
+import EventList from '@/components/EventList'
+import { getEvents } from '@/services/blockchain'
+import { EventStruct } from '@/utils/type.dt'
+import { NextPage } from 'next'
+import Head from 'next/head'
+import { useEffect, useState } from 'react'
+
+const Page: NextPage<{ eventsData: EventStruct[] }> = ({ eventsData }) => {
+  const [end, setEnd] = useState<number>(6)
+  const [count] = useState<number>(6)
+  const [collection, setCollection] = useState<EventStruct[]>([])
+
+  useEffect(() => {
+    setCollection(eventsData.slice(0, end))
+  }, [eventsData, end])
+
+  return (
+    <div>
+      <Head>
+        <title>Event X | Personal</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <EventList events={collection} />
+
+      <div className="mt-10 h-20 "></div>
+
+      {collection.length > 0 && eventsData.length > collection.length && (
+        <div className="w-full flex justify-center items-center">
+          <button
+            className="bg-orange-500 shadow-md rounded-full py-3 px-4
+        text-white duration-300 transition-all"
+            onClick={() => setEnd(end + count)}
+          >
+            {' '}
+            Load More
+          </button>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Page
+
+export const getServerSideProps = async () => {
+  const eventsData: EventStruct[] = await getEvents()
+  return {
+    props: { eventsData: JSON.parse(JSON.stringify(eventsData)) },
+  }
+}
